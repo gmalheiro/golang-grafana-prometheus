422d19884bd7405e0603554fe78f37a4d02f9b3d
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-09-20 12:21:43 -0300

    feat: add info field with version

--- internal/models/metrics.go
+++ internal/models/metrics.go
@@ -4,22 +4,31 @@ import "github.com/prometheus/client_golang/prometheus"
 
 type metrics struct {
 	devices prometheus.Gauge
+	info    *prometheus.GaugeVec
 }
 
 func NewMetrics(reg prometheus.Registerer) *metrics {
 	m := &metrics{
 		devices: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name:      "myApp",
 			Namespace: "connected_devices",
+			Name:      "myApp",
 			Help:      "Number of currently connected devices.",
 		}),
+		info: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "myApp",
+			Name:      "info",
+			Help:      "Information about the My App environment",
+		}, []string{"version"}),
 	}
 
-	reg.MustRegister(m.devices)
+	reg.MustRegister(m.devices, m.info)
 
 	return m
 }
 
 func (m *metrics) Devices() prometheus.Gauge {
 	return m.devices
+}
+func (m *metrics) Info() *prometheus.GaugeVec {
+	return m.info
 }
